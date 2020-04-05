eda9a9ac7cf68d488edbe0a24f63127e35eda7ed
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-04-05 18:54:28 +0000

    add rspec tests for tutorial

---
+++ boat_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'boat'
+
+describe Boat do
+  it 'should create boats' do
+    expect(Boat.new).to be_a Boat
+  end 
+  describe '#allowed_aboard?' do
+    it 'returns true is inventory includes a life jacket' do
+      a_boat = Boat.new
+      allowed = a_boat.allowed_aboard?(['life jacket', 'sun glasses'])
+      expect(allowed).to be true
+    end 
+    it 'returns false if inventory does not include a life jacket' do
+      a_boat = Boat.new
+      allowed = a_boat.allowed_aboard?(['swim trunks', 'flippy floppies'])
+      expect(allowed).to be false
+    end 
+  end 
+end
